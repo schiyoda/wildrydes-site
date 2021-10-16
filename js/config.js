7d22da6a4f5374d7fc99d46d33bd1ace2cb3af63
EC2 Default User <ec2-user@ip-172-31-11-66.ap-northeast-1.compute.internal>
2021-10-16 09:05:23 +0000

    add auth

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_scQzvRul9', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7s960d1am2dql9gfrfruf5hqfb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
